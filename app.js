4f46a263752d8a2e6bd24f8151d71d9dd978c2fe
davidandrus <davidandrus.com@gmail.com>
2017-01-21 18:15:38 -0800

    feat: loading countries and showing loading state

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-function uiViewModel() {
+/**
+ * Gets a list of countries from https://restcountries.eu/rest/v1/all
+ * @return {Promise} the promise returned with when successful the array of countries
+ */
+function getCountries() {
+  return fetch('https://restcountries.eu/rest/v1/all')
+    .then(function(response) {
+      return response.json();
+    });
+}
 
-  this.countries = ko.observableArray([
-    { name: 'United States' },
-    { name: 'Canada' }
-  ]);
+function uiViewModel() {
+  var self = this;
+
+  self.countries = ko.observableArray();
+  self.loading = ko.observable(true);
+  self.error = ko.observable(false);
+
+  getCountries()
+    .then(function(response) {
+      self.loading(false);
+      self.countries(response);
+      console.log(self.countries())
+    })
+    .catch(function(e) {
+      console.log('error yo', e);
+      self.loading(false);
+      self.error(true);
+    });
+
 }
 
 ko.applyBindings(new uiViewModel());
